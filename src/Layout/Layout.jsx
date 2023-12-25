6379085eafc90e313ff7fe5a356b56a2f7373154
endritbejta <endrit.bejta@hotmail.com>
2023-12-25 21:46:48 +0100

    minor changes

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Header from "./Header";
 import classes from "./Layout.module.css";
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
@@ -10,22 +10,30 @@ const Layout = () => {
   const [isRecording, setIsRecording] = useState(true);
   const blobRef = useRef();
   console.log(blobRef);
-  document.body.onpointermove = (event) => {
-    const { clientX, clientY } = event;
-    blobRef.current.style.left = `${clientX}px`;
-    blobRef.current.style.top = `${clientY}px`;
 
-    blobRef.current.animate(
-      {
-        left: `${clientX}px`,
-        top: `${clientY}px`,
-      },
-      {
-        duration: 4000,
-        fill: "forwards",
-      }
-    );
-  };
+  const [clientX, setClientX] = useState(window.innerWidth);
+
+  useEffect(() => {
+    setClientX(window.innerWidth);
+  }, [clientX]);
+  if (clientX > 918) {
+    document.body.onpointermove = (event) => {
+      const { clientX, clientY } = event;
+      blobRef.current.style.left = `${clientX}px`;
+      blobRef.current.style.top = `${clientY}px`;
+
+      blobRef.current.animate(
+        {
+          left: `${clientX}px`,
+          top: `${clientY}px`,
+        },
+        {
+          duration: 4000,
+          fill: "forwards",
+        }
+      );
+    };
+  }
 
   return (
     <div className={classes.Layout}>
